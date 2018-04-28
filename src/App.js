be081f1d9623dd1a6f3c6c2264ffa0c5f5a99dc8
DylanHedges <d.hedges1@hotmail.co.uk>
2018-04-28 07:57:40 -0400

    Added change name input field

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 		otherstate: 'This will not be changed by the onClick event handler'
 	}
 
-	//Defines the function that will be called when clicking the button, this is a method of the class, it is assigned to the class as a property (as a variable), if we dont use this syntax there will be issues when calling "this.switchNameHandler" as "this" wont refer to the class ("App") -> property ("switchNameHandler"); this.state.persons[0].name = 'Sam'; - we are not allowed to mutate state like this
+	//Changes the name when clicking - this function will be called when clicking the button/on text, this is a method of the class, it is assigned to the class as a property (as a variable), if we dont use this syntax there will be issues when calling "this.switchNameHandler" as "this" wont refer to the class ("App") -> property ("switchNameHandler"); this.state.persons[0].name = 'Sam'; - we are not allowed to mutate state like this
 	switchNameHandler = (newName) => {
 		this.setState({
 			persons: [
@@ -24,7 +24,16 @@ class App extends Component {
 			]
 		});
 	}
-
+//Changes the name when typing in the input field - "(event)" - default object that is automatically passed into the function by React; "event.target.value" - the input the user has typed
+nameChangedHandler = (event) => {
+	this.setState({
+		persons: [
+			{ name: 'Max', age: 38},
+			{ name: event.target.value, age: 54},
+			{ name: 'Jane', age: 31}
+		]
+	});
+}
 	//"render()" - React will call this method to render something to the screen, "<div>" this is JSX not HTML; "className" - we use this instead of "class" (for CSS) bcause we use "class" when defining our component; "<Person> </Person>" - everything side of our component tags will be passed as "children" to our component; <div className="App"> - everything must be wrapped in one root element; "this.state.persons[0].name" - refers to the class "App" -> state property (object) -> persons array -> index 0 -> name ('Max'); "onClick()" - in JSX we use a captial "C" for click; //"this.switchNameHandler" - we dont include (), if we did it would mean React would execute it immediately after the DOM was rendered; click={this.switchNameHandler} - this is a method that we pass to the functional component in Person.js, we can then use this method in Person.js by accessing its props;
 	//"this.switchNameHandler.bind(this, 'Max!')" - pass in value to our function to change the name, ".bind(this, )" resolves the ".this" issue, needed so that our "switchNameHandler" function ".this" refers to the "render(){}""; "() => this.switchNameHandler()" - alternative method (not preferred) to passing values to our function, executes a function and returns the result (a name in this case), a "return" keyword will be automatically added if the function is written all in 1 line
 	render() {
@@ -38,7 +47,10 @@ class App extends Component {
 				<Person
 					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
-					click={this.switchNameHandler.bind(this, 'Max!')}/>Hobby: Diving
+					click={this.switchNameHandler.bind(this, 'Max!')}
+					changed={this.nameChangedHandler}
+					>Hobby: Diving
+				</Person>
 				<Person
 					name={this.state.persons[2].name}
 					age={this.state.persons[2].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 //Functional Component - Use where possible, only renders something to the DOM, doesn't mutate state (containers/class-based components mutate state), this is important when the application gets big, the function name is typically lower case and the same name as your file; this JSX syntax is transformed to HTML using "React.createElement()"; {Math.floor(Math.random() * 30)} - if we use { } everything inside will be executed as JS code, if we dont use { } everything inside will be displayed as text; "(props)" - attributes (e.g age="28") passed into this component are known as props (properties);
 const person = (props) => {
-  //{props.name} - this is the syntax for functional components, for class-based components we use "this.props" (we use "props" for functional components and "state" for class-based components), in additon to this we cannot call/edit state e.g "this.setState()" as it is not class based component (it does not "extends Component" and as a result does not know the ".setState()" method); {props.children} - reserved keyword, it returns whatever is inbetween our component tags "<Person> </Person>", this could be anything (even other React components); onClick={props.click} - this is a method that was passed in from the App.js App container, this method is passed into the props of this component, we can use the method by accessing the props -> switchNameHandler (by doing this we can change the state via the container, its still not changed in this component)
+  //{props.name} - this is the syntax for functional components, for class-based components we use "this.props" (we use "props" for functional components and "state" for class-based components), in additon to this we cannot call/edit state e.g "this.setState()" as it is not class based component (it does not "extends Component" and as a result does not know the ".setState()" method); {props.children} - reserved keyword, it returns whatever is inbetween our component tags "<Person> </Person>", this could be anything (even other React components); onClick={props.click} - this is a method that was passed in from the App.js App container, this method is passed into the props of this component, we can use the method by accessing the props -> switchNameHandler (by doing this we can change the state via the container, its still not changed in this component); "onChange={}" - executed whenever the input value changes; "value=props.name" - displays the current name in the input field
   return (
     <div>
       <p onClick={props.click}>Hello i am {props.name} i am {props.age} years old!</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 };
